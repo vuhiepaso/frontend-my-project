4227b49daa2d209f83a35537f536a01ac1647009
vuhiepaso <vuhiepomg@gamail.com>
2023-03-16 17:33:22 +0700

    update

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,37 @@
 <template>
-  <h1>LOGIN</h1>
+  <h3 class="text-3xl txt-login">LOGIN</h3>
+  <div class="mt-1 text-left">
+    <p>User name:</p>
+    <el-input v-model="userName" placeholder="User name" />
+  </div>
+  <div class="mt-1 text-left">
+    <p>Password:</p>
+    <el-input
+      v-model="password"
+      type="password"
+      show-password
+      placeholder="Password"
+    />
+  </div>
+  <el-button @click="onClick" type="danger" class="mt-4">Login</el-button>
 </template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+// declaration of variables
+const userName = ref<string>("");
+const password = ref<string>("");
+let color: string = "red";
+
+function onClick() {
+  color = userName.value;
+  console.log(userName.value);
+  console.log(password.value);
+}
+</script>
+
+<style>
+.txt-login {
+  color: v-bind(color);
+}
+</style>
